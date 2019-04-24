c45e23f4aa3715cc9ede2f85a909c20160fb1a9d
sudheera <catchsudheera@gmail.com>
2019-04-24 22:02:35 +0530

    Added up/down trend indication

--- arduino-water-level.ino
+++ arduino-water-level.ino
@@ -11,9 +11,9 @@ const int heightTank_1 = 78, bufferTank_1 = 30;
 // Sensor unit hight                                   : 3 cm
 // Hight from ball-value's ball to overflow pipe level : 10.5 cm
 
-double duration;
+double duration, pastData = 0, pastAvg = 0;
 float fillPercTank_1;
-int distanceCm;
+int distanceCm, loopCount = 0;
 
 void setup() {
   lcd.begin(16, 2);
@@ -22,6 +22,7 @@ void setup() {
 }
 
 void loop() {
+  loopCount++;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
@@ -31,6 +32,9 @@ void loop() {
 
   duration = pulseIn(echoPin, HIGH);
   distanceCm = duration * 0.0348 / 2;
+  if (distanceCm % 2 != 0) { // we need to lower the precision in order not to fluctuate the output percentage value
+    distanceCm++;
+  }
   fillPercTank_1 = ((heightTank_1 + bufferTank_1 - distanceCm) * 100.0 / (heightTank_1 * 1.0));
 
   if (fillPercTank_1 < 0) {
@@ -54,9 +58,24 @@ void loop() {
   } else {
     lcd.print("HIGH");
   }
+  
+  pastData += distanceCm;
+  pastAvg = pastData / loopCount;
+
+  if (pastAvg > distanceCm) {
+    lcd.print(" D");
+  } else if (pastAvg < distanceCm) {
+    lcd.print(" U");
+  }
 
   lcd.setCursor(0, 1);
   lcd.print(distanceCm);
-
-  delay(300);
+  lcd.print(" ");
+  lcd.print(pastAvg);
+  
+  delay(1000); // No need to update the LCD in very high frequency
+  if (loopCount >= 60) {
+    loopCount = 0;
+    pastData = 0;
+  }
 }
